a98318414058c8ba8fe21f9666dd2903c80c7f3f
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-09-13 20:04:32 +0300

    Сделать бургер меню и все

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,30 +6,30 @@ import cross from '../../images/cross.svg';
 
 
 const MoviesCard = ({card}) => {
-    const location = useLocation();
-const [isSaved, setIsSaved] = React.useState(false);
+  const location = useLocation();
+  const [isSaved, setIsSaved] = React.useState(false);
 
 
 function handleSave() {
-    setIsSaved(!isSaved);
+  setIsSaved(!isSaved);
 }
 
 return (
-<div className='movie-card'>
+  <div className='movie-card'>
     <div className='movie-card__text-container'>
-    <h2 className='movie-card__title'>{card.name}</h2>
-    <p className='movie-card__time'>{card.time}</p>
+      <h2 className='movie-card__title'>{card.name}</h2>
+      <p className='movie-card__time'>{card.time}</p>
     </div>
     <img className='movie-card__image' src={card.image} alt='movie poster'/>
-    {location.pathname === '/saved-movies' &&
+    {location.pathname === '/saved-movies' && 
     <button className='movie-card__button' onClick={handleSave}>
     <img className='movie-card__button-img' alt='delete movie icon' src={cross}/>
     </button>}
     {location.pathname === '/movies' &&
     <button className={isSaved ? 'movie-card__button movie-card__button_red' : 'movie-card__button'}
-            onClick={handleSave}>{isSaved ? <img className='movie-card__button-img' alt='saved movie icon' src={ok}/> :
-        <p className='movie-card__button-text'>Сохранить</p>}</button>}
-</div>
+            onClick={handleSave}>{isSaved ? <img className='movie-card__button-img' alt='saved movie img' src={ok}/> 
+            : <p className='movie-card__button-text'>Сохранить</p>}</button>}
+  </div>
 );
 };
 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,17 +3,18 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 
 const SearchForm = () => {
-return (
-        <section className='search'>
-            <form className='search__form' name='search-bar'>
-                <div className='search__container'>
-                    <input className='search__input' type='text' placeholder='Фильм'/>
-                    <button className='search__button' type='submit'></button>
-                </div>
-                <FilterCheckbox/>
-            </form>
-        </section>
-    );
+  return (
+    <section className='search'>
+      <form className='search__form' name='search-bar'>
+        <div className='search__container'>
+          <div className='search__image-loop'></div>  
+          <input className='search__input' type='text' placeholder='&nbsp; Фильм'/>
+          <button className='search__button' type='submit'><p className='search__button-text'>Найти</p></button>
+          <FilterCheckbox/>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
